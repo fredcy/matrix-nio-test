551bd3b146a4dd5455d6b4f75d54dde54e08df44
Fred Yankowski <fcy@imsa.edu>
2019-07-05 11:51:09 -0500

    save

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+See https://matrix.org/docs/guides/usage-of-matrix-nio
+and https://github.com/poljar/matrix-nio
+"""
+
 from importlib import util
 import asyncio
 from nio import (AsyncClient, SyncResponse, RoomMessageText)
@@ -16,9 +21,15 @@ def main():
         response = await async_client.login(config['client']['password'])
         print(f"login response: {response}")
 
+        with open ("next_batch","r") as next_batch_token:
+            async_client.next_batch = next_batch_token.read()
+
         while (True):
             sync_response = await async_client.sync(30000)
             print(f"sync response: {sync_response}")
+
+            with open("next_batch","w") as next_batch_token:
+                next_batch_token.write(sync_response.next_batch)
 
             if len(sync_response.rooms.join) > 0:
                 joins = sync_response.rooms.join
