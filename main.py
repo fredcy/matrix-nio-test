92c7223b5427b580161504a139b479c43d6185b3
Fred Yankowski <fcy@imsa.edu>
2019-07-05 11:10:49 -0500

    list events on each sync

--- main.py
+++ main.py
@@ -8,17 +8,23 @@ def main():
     config = configparser.ConfigParser()
     config.read("config")
 
-    print(f"client = {config.items('client')}")
+    print(f"{config['client']['homeserver'], config['client']['username']}")
 
     async_client = AsyncClient(config['client']['homeserver'], config['client']['username'])
 
     async def main():
         response = await async_client.login(config['client']['password'])
-        print(response)
+        print(f"login response: {response}")
 
         while (True):
             sync_response = await async_client.sync(30000)
-            print(sync_response)
+            print(f"sync response: {sync_response}")
+
+            if len(sync_response.rooms.join) > 0:
+                joins = sync_response.rooms.join
+                for room_id in joins:
+                    for event in joins[room_id].timeline.events:
+                        print(f"event: {event}")
 
     asyncio.run(main())
 
